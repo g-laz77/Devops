e1ae206d9600b9ccee6ea8b955f7181ef442a706
g-laz77 <suhanprabhu@gmail.com>
2017-07-19 01:27:11 +0530

    fixed checksum of a directory

--- checksum.sh
+++ checksum.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
-#logf="$1.log"
-interval=2000
-first_run=
-# temp files, current and last md5s for diff to compare
-temp="$(mktemp ./temp/$1.XXXXXX)"
-if [ -z "$1" ]; then
-        echo "No file specified, aborting" >&2
-        exit 1
-fi
-echo "Watching at ${interval}s intervals:   $1"
-FILES=./*
-for f in $FILES
+echo "eh"
+for f in $("ls")
 do
-   temp="$(mktemp ./temp/$f.XXXXXX)"
-   md5sum "$1" > $temp
+   echo "$f"
+   touch temp/$f.md5
+   md5 "$f" > temp/$f.md5
 done
 
 #  clamscan -r .|tail -n 9       #call the antivirus program
