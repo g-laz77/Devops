f181cf3a95591cf7d7673689989e8460869313e6
g-laz77 <suhanprabhu@gmail.com>
2017-07-20 00:38:37 +0000

    added ansible .yml for checksum

--- checksum.sh
+++ checksum.sh
@@ -4,7 +4,7 @@ for f in $("ls")
 do
    echo "$f"
    touch temp/$f.md5
-   md5 "$f" > temp/$f.md5
+   md5sum "$f" > temp/$f.md5
 done
 
 #  clamscan -r .|tail -n 9       #call the antivirus program
