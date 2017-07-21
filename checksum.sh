27ff677abad10b57e33a7c6caf47dd709fea2015
g-laz77 <suhanprabhu@gmail.com>
2017-07-21 00:37:26 +0000

    checksum play added

--- checksum.sh
+++ checksum.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "eh"
+cd /root/website
+mkdir checksum
 for f in $("ls")
 do
    echo "$f"
    touch temp/$f.md5
-   md5sum "$f" > temp/$f.md5
+   md5sum "$f" > checksum/$f.md5
 done
-
-#  clamscan -r .|tail -n 9       #call the antivirus program
